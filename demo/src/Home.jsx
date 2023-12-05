2e0e13d6592b00eebe7908a6ba952c038b653354
Varshakadam19 <Varshakadam@opticalarc.com>
2023-12-05 12:55:25 +0530

    add

--- demo/src/Home.jsx
+++ demo/src/Home.jsx
@@ -3,8 +3,8 @@ import './home.css'
 const Home = () => {
   return (
     <>
-    <p>Home</p>
     <h1>Hello!!</h1>
+    <h2>Welcome to React world</h2>
     <button className='b1'>Click here!</button>
     </>
     
