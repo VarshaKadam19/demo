f09672dd73dc41c32a039e81c911f41671af214b
Varshakadam19 <Varshakadam@opticalarc.com>
2023-12-05 11:58:17 +0530

    add commit

---
+++ demo/src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './home.css'
+const Home = () => {
+  return (
+    <>
+    <p>Home</p>
+    <h1>Hello!!</h1>
+    <button className='b1'>Click here!</button>
+    </>
+    
+  )
+}
+
+export default Home
